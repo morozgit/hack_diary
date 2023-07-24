6ce165b86fff447aaaebfc6ad5d0581d8e85ba97
morozgit <morozec76@gmail.com>
2023-07-24 17:17:31 +0300

    exception handling and reduced number of requests

--- hack_diary.py
+++ hack_diary.py
@@ -1,4 +1,6 @@
 import django
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import MultipleObjectsReturned
 from django.conf import settings
 import random
 settings.configure()
@@ -12,24 +14,47 @@ from datacenter.models import Commendation
 from datacenter.models import Teacher
 
 
-schoolkid = input()
-subject = input()
-praise = ['Молодец!', 'Это как раз то, что нужно!', 'Ты на верном пути!', 'Ты многое сделал, я это вижу!']
+PRAISE  = ['Молодец!',
+           'Это как раз то, что нужно!',
+           'Ты на верном пути!',
+           'Ты многое сделал, я это вижу!']
+
 
 def fix_marks(schoolkid):
-	child = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
-	for point in child:
-		point.points = 5 
-		point.save()
+    student_grades = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
+    student_grades.update(points=5)
 
 
 def remove_chastisements(schoolkid):
-	chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
-	chastisement.delete()
+    chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisement.delete()
 
 
 def create_commendation(schoolkid, subject):
-	schoolchild = Schoolkid.objects.get(full_name__contains=schoolkid)
-	subject_6A = Lesson.objects.filter(subject__title__contains=subject, year_of_study__contains=6, group_letter__contains="А").first()
-	Commendation.objects.create(text=random.choice(praise), created=subject_6A.date, schoolkid=schoolchild, subject=subject_6A.subject, teacher=subject_6A.teacher)
-	remove_chastisements(schoolkid)
+    try:
+    	schoolchild = Schoolkid.objects.get(full_name__contains=schoolkid)
+    except MultipleObjectsReturned:
+        print('Найдено больше одного. Введите ФИО')
+    except ObjectDoesNotExist:
+        print('Не найдено такого имени')
+    class_lesson = Lesson.objects.filter(subject__title__contains=subject,
+                                         year_of_study__contains=6,
+                                         group_letter__contains="А").order_by('year_of_study').first()
+    Commendation.objects.create(text=random.choice(PRAISE),
+                                created=class_lesson.date,
+                                schoolkid=schoolchild,
+                                subject=class_lesson.subject,
+                                teacher=class_lesson.teacher)
+    remove_chastisements(schoolkid)
+
+
+def main():
+    schoolkid = input()
+    subject = input()
+    fix_marks(schoolkid)
+    remove_chastisements(schoolkid)
+    create_commendation(schoolkid, subject)
+
+
+if __name__ == '__main__':
+    main()
